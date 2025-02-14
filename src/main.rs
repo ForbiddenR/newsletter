f81ba48fbcfa1ba5f66fe5d275b0abc97a5af5a5
ForbiddenR <hostyouposition@outlook.com>
2025-02-14 16:37:50 +0800

    trace the http server

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,23 @@
 use std::net::TcpListener;
 
-use newsletter::{configuration::get_configuration, startup::run};
+use newsletter::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
+use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // We removed the `env_logger` line we had before!
+    let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
-    // We are falling back to printing all spans at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("newsletter".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(&configuration.database.connection_string())
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to Postgres.");
     // We have removed the hard=coded `8000` - it's now coming from our settings!
     let address = format!(
@@ -31,5 +25,6 @@ async fn main() -> std::io::Result<()> {
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,12 +11,9 @@ pub struct FormData {
 }
 
 // Let's start simple: we always return a 200 OK
+#[tracing::instrument(name = "Adding a new subscriber", skip(form, pool), fields(subscriber_email= %form.email, subscriber_name = %form.name))]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!("Adding a new subscriber.", %request_id, subscriber_email= %form.email, subscriber_name = %form.name);
-    let _request_span_guard = request_span.enter();
-
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+    let request_span = tracing::info_span!("Saving new subscriber details in the database.");
 
     match sqlx::query!(
         r#"insert into subscriptions (id, email, name, subscribed_at) values ($1, $2, $3, $4)"#,
@@ -26,17 +23,34 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now(),
     )
     .execute(pool.get_ref())
-    .instrument(query_span)
+    .instrument(request_span)
     .await
     {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"insert into subscriptions (id, email, name, subscribed_at) values ($1, $2, $3, $4)"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now(),
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
